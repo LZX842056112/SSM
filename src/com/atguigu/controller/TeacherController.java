3f55f52c64f36921d3fbb45abc820b6b926f32f8
李宗效 <842056112@qq.com>
2020-03-25 18:05:08 +0800

    查询教师信息-SSM整合练习

---
+++ src/com/atguigu/controller/TeacherController.java
@@ -0,0 +1,25 @@
+package com.atguigu.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.atguigu.bean.Teacher;
+import com.atguigu.service.TeacherService;
+
+@Controller
+public class TeacherController {
+	
+	@Autowired
+	TeacherService teacherService;
+	
+	@RequestMapping("/getTea")
+	public String getTeacher(@RequestParam(value="id",defaultValue="1")Integer id,Model model) {
+		Teacher t = teacherService.getTeacher(id);
+		model.addAttribute("teacher", t);
+		return "success";
+	}
+
+}
---
+++ src/com/atguigu/dao/TeacherDao.java
@@ -0,0 +1,9 @@
+package com.atguigu.dao;
+
+import com.atguigu.bean.Teacher;
+
+public interface TeacherDao {
+
+	public Teacher getTeacherById(Integer id);
+	
+}
